e819c5bc414d60a0ad781af4427a3dcef202d235
Syed <amanxsyed@gmail.com>
2024-05-27 20:04:21 +0500

    Correct some minor mistakes

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const generateBtn = document.querySelector(".form button");
 const qrInput = document.querySelector(".form input");
 const qrImg = document.querySelector(".qr-code img");
 
-
-generateBtn.addEventListener("click", () => {
-    let qrValue = qrInput.value;
-    // if the input value is empty then return from here
-	if(!qrValue)return;
-	// getting a QR code of user entered value using the 
+// Function to generate QR code
+function generateQRCode() {
+  let qrValue = qrInput.value;
+  // if the input value is empty then return from here
+  if (!qrValue) return;
+  // getting a QR code of user entered value using the 
 	// qrserver api and passing the api returned img src to qrImg
-	qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`;
-	wrapper.classList.add("active");
-});
+  qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`;
+  wrapper.classList.add("active");
+}
 // Event listener for button click
 generateBtn.addEventListener("click", generateQRCode);
 
